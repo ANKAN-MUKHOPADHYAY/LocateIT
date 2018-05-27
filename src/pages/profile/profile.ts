ae4a43f7dc86ac211e01ea9e74eb9ee46eacb185
Aruna <aruna.aru94@gmail.com>
2018-05-27 14:29:32 +0530

    Added profile page look and feel

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { HttpServiceProvider } from '../../providers/http-service/http-service';
 /**
  * Generated class for the ProfilePage page.
  *
@@ -8,30 +8,37 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * Ionic pages and navigation.
  */
 
-@IonicPage()
-@Component({
-  selector: 'page-profile',
-  templateUrl: 'profile.html',
-})
+ @IonicPage()
+ @Component({
+   selector: 'page-profile',
+   templateUrl: 'profile.html',
+ })
 
-export class ProfilePage {
-	fileToUpload: File = null;
-	info : string = "Personal";
+ export class ProfilePage {
+   fileToUpload: File = null;
+   info : string = "Personal";
 
-  userinfo : { u_fname : string, u_lname : string, u_dob : string, u_email : string, u_mobile : string}={u_fname : '', u_lname : '', u_dob : '', u_email: '', u_mobile: ''};
+   public userproinfo : any = [];
 
 
-  
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+   constructor(public navCtrl: NavController, public navParams: NavParams,public _restService: HttpServiceProvider) {
+   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ProfilePage');
-  }
+   ionViewDidLoad() {
+     console.log('ionViewDidLoad ProfilePage');
 
-  handleFileInput(files: FileList) {
-    this.fileToUpload = files.item(0);
-}
+     //this.userproinfo = {"status":true,"result":{"user_id":79,"user_first_name":"ALOO","user_last_name":"GOBI","user_mobile_number":4141414141,"user_altmobile_number":4141414141,"user_email":"ALOO.GOBI@GMAIL.COM","user_type":"Student","active_status":1,"uniqueid":6,"username":"aloogobi","subscribe_message":0,"subscribe_call":1,"subscribe_mail":1,"permanent_address":null,"current_address":"Kumaraswamy Layout","city":"Bangalore","state":"Karnataka"}};
+     this._restService.get('/user/userinfo/'+sessionStorage.getItem('userid')).then( res => {
+       console.log(sessionStorage.getItem('userid'));
+       if(res.status == true){
+         this.userproinfo = res.result;
+         console.log(this.userproinfo);
+       }
+     });
+   }
+  // handleFileInput(files: FileList) {
+  //   this.fileToUpload = files.item(0);
+  // }
 
 
 }
