0ae2a1c0093179d11ef3def265d74f7d15ea0707
ankantechninzaz <techninzazankan@gmail.com>
2018-09-15 18:00:16 +0530

    Added admin operations page

---
+++ src/pages/admin-add-ops/admin-add-ops.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { AdminAddOpsPage } from './admin-add-ops';
+
+@NgModule({
+  declarations: [
+    AdminAddOpsPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(AdminAddOpsPage),
+  ],
+})
+export class AdminAddOpsPageModule {}
