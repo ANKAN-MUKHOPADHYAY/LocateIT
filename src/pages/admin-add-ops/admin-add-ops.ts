11592711818615b2fab2ab5871c3329c1005bce7
Poorna Chandra Tejas.J <poornachandratejas.j@MacBook-Air.local>
2018-09-16 10:30:26 +0530

    Added code for add new course

--- src/pages/admin-add-ops/admin-add-ops.ts
+++ src/pages/admin-add-ops/admin-add-ops.ts
@@ -16,11 +16,17 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class AdminAddOpsPage {
   info : string = "newCourse";
 
-  userinfo : { u_fname : string, u_lname: string, u_Iname: string, u_mobile: string,u_altmobile: string, u_email: string, u_type: string,u_password: string} = { u_fname : '', u_lname: '', u_Iname:'', u_mobile: '',u_altmobile: '', u_email: '', u_type: '',u_password: ''};
+  courseDetails : { course_name : string, imageurl: any, createdby: any, course_status: any, parentcategory: any} = { course_name : '', imageurl: '', createdby:'admin@techninzaz.com', course_status: 1, parentcategory: ''};
 
   userdetail:{u_id : string, u_cid : string, u_lid : string} ={u_id : '', u_cid : '', u_lid : ''};
 
+ 
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  addNewCourseProcess(){
+    console.log(this.courseDetails);
   }
 
   ionViewDidLoad() {
