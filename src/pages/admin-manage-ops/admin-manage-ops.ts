bc03a48707472a5ac15e1ee27479c3d886f9e9a8
Rajath K R <rajathkr1996@gmail.com>
2018-09-16 12:59:43 +0530

    Added location get data

--- src/pages/admin-manage-ops/admin-manage-ops.ts
+++ src/pages/admin-manage-ops/admin-manage-ops.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { HttpServiceProvider } from '../../providers/http-service/http-service';
 /**
  * Generated class for the AdminManageOpsPage page.
  *
@@ -16,12 +16,17 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class AdminManageOpsPage {
   info : string = "newCourse";
   infor: string ="newLocation";
+  offeredLocation : any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public _restService: HttpServiceProvider) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AdminManageOpsPage');
+    this._restService.get('/search/alllocation').then(resp => {
+      console.log(resp);
+      this.offeredLocation = resp.response;
+    });
   }
 
 }
