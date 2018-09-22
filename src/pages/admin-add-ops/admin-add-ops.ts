dc246275bdc4555a33f8eebe1d1cc7624c719326
callmeankush <ankush.1234singh@gmail.com>
2018-09-22 11:17:34 +0530

    admin page

--- src/pages/admin-add-ops/admin-add-ops.ts
+++ src/pages/admin-add-ops/admin-add-ops.ts
@@ -14,12 +14,8 @@ export class AdminAddOpsPage {
 
   courseDetails: { course_name: string, imageurl: any, createdby: any, course_status: any, parentcategory: any } = { course_name: '', imageurl: '', createdby: 'admin@techninzaz.com', course_status: 1, parentcategory: '' };
 
-<<<<<<< HEAD
 
   userdetail:{u_id : string, u_cid : string, u_lid : string} ={u_id : '', u_cid : '', u_lid : ''};
-=======
-  //userdetail:{u_id : string, u_cid : string, u_lid : string} ={u_id : '', u_cid : '', u_lid : ''};
->>>>>>> d67861e66e2da39fb3d0e78b37b5acaf623f438c
 
   LocationInfo: { Location_name: string, Location_city: string, Location_state: string, Location_pincode: string, Location_createdby: string, Location_relevant_name: string, Location_nearby: string } = { Location_name: '', Location_city: '', Location_state: '', Location_pincode: '', Location_createdby: '', Location_relevant_name: '', Location_nearby: '' }
 
