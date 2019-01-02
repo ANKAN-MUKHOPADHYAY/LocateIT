b7ef89ed1b7e520b5d589f7835db19959f3444bf
Poorna Chandra Tejas.J <poornachandratejas.j@MacBook-Air.local>
2019-01-02 21:47:15 +0530

    Added splashscreen and modified terms page

--- src/components/footer/footer.ts
+++ src/components/footer/footer.ts
@@ -13,18 +13,10 @@ export class FooterComponent {
     console.log('Hello FooterComponent Component');
     this.text = 'Hello World';
   }
-<<<<<<< HEAD
   gotoTerms(){
   	this.navCtrl.push('TermsConditionsPage');
   }
   gotoprivacy(){
     this.navCtrl.push('PrivacyPolicyPage');
   }
-=======
-
-  gotoPrivacy(){
-  	this.navCtrl.push('PrivacyPage');
-  }
-
->>>>>>> da80c1a7f768ce51563eb3a71bb1a7476ab74b31
 }
